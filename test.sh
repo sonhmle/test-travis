6824fc9de80488fa722e3a6f6093469f753774ec
sonhmle <sonhmle@kms-technology.com>
2022-02-11 10:10:55 +0700

    Implement execute test

---
+++ test.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+set -euo pipefail
+
+printf "\n=====================================================================================\n"
+printf "KOBITON EXECUTE TEST PLUGIN"
+printf "\n=====================================================================================\n\n"
+
+chmod +x ./execute-test/app-ubuntu
+
+./app-ubuntu
